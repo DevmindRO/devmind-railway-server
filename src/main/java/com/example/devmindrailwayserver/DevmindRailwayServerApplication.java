414a2a97802a3620b54ab0aafafb9a4cd85f4b88
Cristi Nica <nicacristi20@gmail.com>
2023-06-28 19:06:58 +0300

    initial commit

---
+++ src/main/java/com/example/devmindrailwayserver/DevmindRailwayServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.devmindrailwayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevmindRailwayServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevmindRailwayServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/devmindrailwayserver/DevmindRailwayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devmindrailwayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevmindRailwayServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
